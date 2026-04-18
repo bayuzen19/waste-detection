5a4baf1fc51d5f386dd3c33d5768206a42a6f916
bayuzen19 <bayuzen19@gmail.com>
2026-04-18 12:21:52 +0700

    [FEAT] add research.ipynb yolo model custom yolov5s

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="waste detection",
+    version="0.0.1",
+    author="Bayuzen",
+    author_email="bayuzen19@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
